ae73c4b3e191ee1a21ada034240f428dc0a8f0aa
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-04-12 23:59:36 -0300

    Criando a Tela de Listagem Financeira

--- MauiAppControleServicos/ViewModels/AgendamentosViewModel.cs
+++ MauiAppControleServicos/ViewModels/AgendamentosViewModel.cs
@@ -4,51 +4,32 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Maui.Controls;
+using ControleServicos.Models;
 
 namespace ControleServicosApp.ViewModels
 {
     public class AgendamentosViewModel : BindableObject
     {
-        public ObservableCollection<Agendamento> Agendamentos { get; set; }
-        public ICommand AdicionarAgendamentoCommand { get; }
-        public ICommand EditarAgendamentoCommand { get; }
-        public ICommand ExcluirAgendamentoCommand { get; }
-
-        public AgendamentosViewModel()
-        {
-            Agendamentos = new ObservableCollection<Agendamento>();
-            AdicionarAgendamentoCommand = new Command(async () => await AdicionarAgendamento());
-            EditarAgendamentoCommand = new Command<Agendamento>(async (agendamento) => await EditarAgendamento(agendamento));
-            ExcluirAgendamentoCommand = new Command<Agendamento>(async (agendamento) => await ExcluirAgendamento(agendamento));
-
-            _ = CarregarAgendamentos();
-        }
-
-        private async Task CarregarAgendamentos()
-        {
-            var lista = await App.Database.ListarTodosAsync<Agendamento>();
-            Agendamentos.Clear();
-            foreach (var agendamento in lista)
-                Agendamentos.Add(agendamento);
-        }
+        // ...
 
         private async Task AdicionarAgendamento()
         {
-            await Application.Current.MainPage.Navigation.PushAsync(new CadastroAgendamentoPage());
+            var mainPage = Application.Current?.Windows.FirstOrDefault()?.Page; // Corrigindo CS0618 e CS8602
+            if (mainPage != null)
+            {
+                await mainPage.Navigation.PushAsync(new CadastroAgendamentoPage());
+            }
         }
 
         private async Task EditarAgendamento(Agendamento agendamento)
         {
-            await Application.Current.MainPage.Navigation.PushAsync(new CadastroAgendamentoPage(agendamento));
-        }
-
-        private async Task ExcluirAgendamento(Agendamento agendamento)
-        {
-            if (agendamento != null)
+            Page? mainPage = Application.Current?.Windows.FirstOrDefault()?.Page; // Corrigindo CS0618 e CS8602
+            if (mainPage != null)
             {
-                await App.Database.DeletarAsync(agendamento);
-                Agendamentos.Remove(agendamento);
+                await mainPage.Navigation.PushAsync(new CadastroAgendamentoPage(agendamento));
             }
         }
+
+        // ...
     }
 }
---
+++ MauiAppControleServicos/ViewModels/CadastroFinanceiroViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ControleServicosApp.Models;
+using ControleServicosApp.Database;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+
+namespace ControleServicosApp.ViewModels
+{
+    public class CadastroFinanceiroViewModel : BindableObject
+    {
+        public Financeiro RegistroFinanceiro { get; set; }
+        public ICommand SalvarRegistroCommand { get; }
+
+        public CadastroFinanceiroViewModel()
+        {
+            RegistroFinanceiro = new Financeiro();
+            SalvarRegistroCommand = new Command(async () => await SalvarRegistro());
+        }
+
+        private async Task SalvarRegistro()
+        {
+            await App.Database.InserirAsync(RegistroFinanceiro);
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
