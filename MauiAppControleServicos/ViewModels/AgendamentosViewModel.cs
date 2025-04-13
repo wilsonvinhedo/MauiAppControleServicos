fcb6796c7152c6995785380959d0ce09112eeca1
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-04-12 23:28:29 -0300

    Implementação do AppControleServicos; Criando a Tela de Listagem de Agendamentos

---
+++ MauiAppControleServicos/ViewModels/AgendamentosViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ControleServicosApp.Models;
+using ControleServicosApp.Database;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace ControleServicosApp.ViewModels
+{
+    public class AgendamentosViewModel : BindableObject
+    {
+        public ObservableCollection<Agendamento> Agendamentos { get; set; }
+        public ICommand AdicionarAgendamentoCommand { get; }
+        public ICommand EditarAgendamentoCommand { get; }
+        public ICommand ExcluirAgendamentoCommand { get; }
+
+        public AgendamentosViewModel()
+        {
+            Agendamentos = new ObservableCollection<Agendamento>();
+            AdicionarAgendamentoCommand = new Command(async () => await AdicionarAgendamento());
+            EditarAgendamentoCommand = new Command<Agendamento>(async (agendamento) => await EditarAgendamento(agendamento));
+            ExcluirAgendamentoCommand = new Command<Agendamento>(async (agendamento) => await ExcluirAgendamento(agendamento));
+
+            _ = CarregarAgendamentos();
+        }
+
+        private async Task CarregarAgendamentos()
+        {
+            var lista = await App.Database.ListarTodosAsync<Agendamento>();
+            Agendamentos.Clear();
+            foreach (var agendamento in lista)
+                Agendamentos.Add(agendamento);
+        }
+
+        private async Task AdicionarAgendamento()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new CadastroAgendamentoPage());
+        }
+
+        private async Task EditarAgendamento(Agendamento agendamento)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new CadastroAgendamentoPage(agendamento));
+        }
+
+        private async Task ExcluirAgendamento(Agendamento agendamento)
+        {
+            if (agendamento != null)
+            {
+                await App.Database.DeletarAsync(agendamento);
+                Agendamentos.Remove(agendamento);
+            }
+        }
+    }
+}
---
+++ MauiAppControleServicos/ViewModels/CadastroAgendamentoViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ControleServicosApp.Models;
+using ControleServicosApp.Database;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+
+namespace ControleServicosApp.ViewModels
+{
+    public class CadastroAgendamentoViewModel : BindableObject
+    {
+        public Agendamento Agendamento { get; set; }
+        public ObservableCollection<Cliente> Clientes { get; set; }
+        public ObservableCollection<Servico> Servicos { get; set; }
+        public ICommand SalvarAgendamentoCommand { get; }
+
+        public CadastroAgendamentoViewModel()
+        {
+            Agendamento = new Agendamento();
+            Clientes = new ObservableCollection<Cliente>();
+            Servicos = new ObservableCollection<Servico>();
+
+            SalvarAgendamentoCommand = new Command(async () => await SalvarAgendamento());
+
+            _ = CarregarDados();
+        }
+
+        private async Task CarregarDados()
+        {
+            Clientes.Clear();
+            Servicos.Clear();
+
+            var listaClientes = await App.Database.ListarTodosAsync<Cliente>();
+            var listaServicos = await App.Database.ListarTodosAsync<Servico>();
+
+            foreach (var item in listaClientes) Clientes.Add(item);
+            foreach (var item in listaServicos) Servicos.Add(item);
+        }
+
+        private async Task SalvarAgendamento()
+        {
+            await App.Database.InserirAsync(Agendamento);
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
